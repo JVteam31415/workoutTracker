28fd1b5a30e6c800b4754dbefdff2d9678037b92
James VanderKam <jamesvanderkam@verizon.net>
2021-04-11 17:33:41 -0400

    port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const mongoose = require("mongoose");
 const apiroutes = require("./routes/apiroutes");
 
-const PORT = 3000;
+const PORT = 3000 ||process.env.PORT;
 
 const app = express();
 
